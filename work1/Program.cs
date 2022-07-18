ed1c37b277f615be559c3bc5b9447d3a1d6d7d84
Galina Gorbunova <galinagorbunova542@gmail.com>
2022-07-18 14:43:20 +0400

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число");
+int chislo = int.Parse(Console.ReadLine());
+
+int chislo2 = ((chislo/10)%10);
+
+Console.Write(chislo2);
