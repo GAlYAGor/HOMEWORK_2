912ba1b62e83a5c26fe6b8fff84652672b9be001
Galina Gorbunova <galinagorbunova542@gmail.com>
2022-07-18 22:07:30 +0400

    work2

--- work2/Program.cs
+++ work2/Program.cs
@@ -5,8 +5,9 @@ int number = int.Parse(Console.ReadLine());
 string number1 = Convert.ToString(number);
 if (number1.Length > 2)
 {
-  Console.WriteLine("третья цифра:" + number1[2]);
+    Console.WriteLine("третья цифра:" + number1[2]);
 }
-else {
-  Console.WriteLine("третьей цифры нет");
+else
+{
+    Console.WriteLine("третьей цифры нет");
 }
